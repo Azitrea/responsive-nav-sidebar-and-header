271aebe105269e668d14e2b2f0503387d40009d9
Azitrea <xyzddz@gmail.com>
2021-07-23 21:40:04 +0200

    nav1 responsive done

--- src/app/nav1/nav1.component.ts
+++ src/app/nav1/nav1.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MediaSize } from 'src/model/media-size.enum';
+import { ResponsiveService } from 'src/service/responsive.service';
 
 @Component({
   selector: 'app-nav1',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav1.component.scss']
 })
 export class Nav1Component implements OnInit {
+  isOpen: boolean;
 
-  constructor() { }
+  subscription: Subscription;
+
+  constructor(private responsiveService: ResponsiveService) { }
 
   ngOnInit(): void {
+    this.subscription = this.responsiveService.screenWidth$.subscribe(screenSize => {
+      if (screenSize >= MediaSize.MD) {
+        this.isOpen = false;
+      }
+    });
+  }
+
+  toggleSideNav(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
